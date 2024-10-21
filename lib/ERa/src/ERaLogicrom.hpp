527d4ab9521a0daa233d0d7ecc3051f4f725248d
PLGdevo <minhphong270704@gmail.com>
2024-10-21 10:59:22 +0700

    Delete lib/ERa directory

--- lib/ERa/src/ERaLogicrom.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INC_ERA_LOGICROM_HPP_
-#define INC_ERA_LOGICROM_HPP_
-
-#define ERA_MODBUS
-
-#include <ERaSimpleLogicrom.hpp>
-
-#endif /* INC_ERA_LOGICROM_HPP_ */
--- lib/ERa/src/ERaRealtekWiFi.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef INC_ERA_REALTEK_WIFI_HPP_
-#define INC_ERA_REALTEK_WIFI_HPP_
-
-#define ERA_MODBUS
-
-#include <ERaSimpleRealtekWiFi.hpp>
-
-#endif /* INC_ERA_REALTEK_WIFI_HPP_ */
